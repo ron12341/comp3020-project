77ce9d9c1f2c12bc8defd4ee32315940977e38c1
ron12341 <arvincruz7524@gmail.com>
2024-11-16 22:02:25 -0600

    created component for customizing image/logo

--- winnipeg-clothes/src/main.tsx
+++ winnipeg-clothes/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+<<<<<<< Updated upstream
 import NavBar from './components/navbar.tsx'
 import CustomizePage from './CustomizePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CustomizePage />
+=======
+import DisplayPreview from './DisplayPreview.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <DisplayPreview />
+>>>>>>> Stashed changes
   </StrictMode>,
 )
